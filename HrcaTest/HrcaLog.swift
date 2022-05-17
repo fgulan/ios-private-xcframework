beaaee06660c8ef2cd3c0e067f4141a7d4523840
Filip Gulan <gulan.filip@gmail.com>
2022-05-17 20:25:44 +0200

    add project

---
+++ HrcaTest/HrcaLog.swift
@@ -0,0 +1,15 @@
+//
+//  HrcaLog.swift
+//  HrcaTest
+//
+//  Created by Filip Gulan on 17.05.2022..
+//
+
+import Foundation
+
+public class HrcaLogger {
+    
+    public static func logSome() {
+        print("Hrca test")
+    }
+}
---
+++ xcframework/HrcaTest.xcframework/ios-arm64/HrcaTest.framework/Headers/HrcaTest.h
@@ -0,0 +1,18 @@
+//
+//  HrcaTest.h
+//  HrcaTest
+//
+//  Created by Filip Gulan on 17.05.2022..
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HrcaTest.
+FOUNDATION_EXPORT double HrcaTestVersionNumber;
+
+//! Project version string for HrcaTest.
+FOUNDATION_EXPORT const unsigned char HrcaTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HrcaTest/PublicHeader.h>
+
+
